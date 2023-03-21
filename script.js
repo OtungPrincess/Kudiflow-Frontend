d667c76aebdaf5295fc254e914549b0ed18f6cbd
Adunni <otungprincess@gmail.com>
2023-03-21 17:35:02 +0100

    mobile view

---
+++ script.js
@@ -0,0 +1,20 @@
+//eye-icon script
+
+    function togglePasswordVisibility() {
+        const passwordInput = document.getElementById("password");
+        const passwordToggle = document.querySelector(".password-toggle");
+  
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            passwordToggle.innerHTML = '<i class="fa fa-eye-slash"></i>';
+        } else {
+            passwordInput.type = "password";
+            passwordToggle.innerHTML = '<i class="fa fa-eye"></i>';
+        }
+    }
+// return button script
+    const backButton = document.querySelector('.return-button');
+
+    backButton.addEventListener('click', () => {
+        window.history.back();
+    });
